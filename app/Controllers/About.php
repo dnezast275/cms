19c89acdda39f4c0c00a0160b1a1b7b42c32d01e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2020-11-01 23:35:15 +0700

    Add category from db

--- app/Controllers/About.php
+++ app/Controllers/About.php
@@ -4,10 +4,13 @@ namespace App\Controllers;
 
 class About extends BaseController
 {
+
     public function index()
     {
+
         $data = [
             'title'        => 'About Us | dNezast',
+            'category'     => $this->category,
         ];
 
         return view('blog/about', $data);
