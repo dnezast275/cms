502c67f3c8338ee0ab63c64b866f85bbfe40d076
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2020-12-04 19:07:40 +0700

    Add post to homepage frontend

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,9 +7,11 @@ class Home extends BaseController
 
 	public function index()
 	{
+		$postData = $this->postModel->findAll();
 		$data = [
 			'title'		=> 'dNezast',
 			'category'	=> $this->category,
+			'postData'	=> $postData,
 		];
 
 		return view('blog/index', $data);
